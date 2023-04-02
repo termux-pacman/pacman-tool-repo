e9b8679cbe546a0f673ca128aaa326860defab7a
Maxython <mixython@gmail.com>
2023-04-02 14:00:48 +0300

    pacman-tool-repo-v1.1: fewer modules and better security

--- index.js
+++ index.js
@@ -2,28 +2,23 @@
 
 const exec = require('@actions/exec');
 const fs = require('fs');
-const request = require('request');
+var https = require('follow-redirects').https;
 const process = require('process');
 const os = require('os');
 const path = require('path');
 
-function download() {
-	return new Promise(function (resolve) {
-		request("https://github.com/termux-pacman/pacman-tool-repo/raw/main/pacman-tool-repo.tar.xz", function () {
-			resolve(null);
-		}).pipe(fs.createWriteStream("pacman-tool-repo.tar.xz"));
+tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "ptr"));
+process.chdir(tmpDir);
+const file = fs.createWriteStream("pacman-tool-repo.tar.xz");
+const request = https.get("https://github.com/termux-pacman/pacman-tool-repo/raw/main/pacman-tool-repo.tar.xz", function(response) {
+	response.pipe(file);
+	file.on("finish", async() => {
+		file.close();
+		await exec.exec("tar xJf pacman-tool-repo.tar.xz");
+		fs.unlinkSync("pacman-tool-repo.tar.xz");
+		process.chdir("pacman-tool-repo/");
+		await exec.exec("sudo ./setup.sh");
+		process.chdir(__dirname);
+		fs.rmSync(tmpDir, {recursive:true, force:true});
 	});
-}
-
-async function main() {
-	tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "ptr"));
-	await download();
-	await exec.exec("tar xJf pacman-tool-repo.tar.xz");
-	fs.unlinkSync("pacman-tool-repo.tar.xz");
-	process.chdir("pacman-tool-repo/");
-	await exec.exec("sudo ./setup.sh");
-	process.chdir(__dirname);
-	fs.rmSync(tmpDir, {recursive:true, force:true});
-}
-
-main();
+});
